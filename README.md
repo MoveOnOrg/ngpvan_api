f3c6409eeca4aa5e492a5ce7775762d929cd999b
Scott Reynen <scott.reynen@moveon.org>
2016-09-14 14:52:47 -0600

    initial code, events and signups

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # NGPVAN API
 
 Python wrapper for NGPVAN API.
+
+## Example Use
+
+`from ngpvan_api.event import NGPVANEventAPI
+
+ngpvan_event_api = NGPVANEventAPI({
+    'NGPVAN_BASE_URL': 'https://api.securevan.com/v4/',
+    'NGPVAN_API_KEY': 'api-key-example|1',
+    'NGPVAN_API_APP': 'api-app',
+})
+
+events = ngpvan_event_api.get_events_by_type_name('Canvass').get('events')
+
+for event in events:
+    signups = ngpvan_event_api.get_signups_for_event(event_id, params={'role':'Participant'}).get('signups')`
---
+++ ngpvan_api/signup.py
@@ -0,0 +1,17 @@
+from ngpvan import base
+
+class NGPVANSignupAPI(base.NGPVANAPI):
+
+    def get_signups(self, page_number=False, params={}):
+        if params.get('role', False):
+            # Filter by fake role param after request
+            role = params.get('role', '')
+            del params['role']
+            signups = self.get_signups(page_number, params)
+            signups['signups'] = [
+                i for i in signups.get('signups', [])
+                if i.get('role', {}).get('name', '') == role
+            ]
+            return signups
+        else:
+            return self.get_page_or_pages('signups', page_number, params, 'signups')
