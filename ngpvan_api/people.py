75d2d200d8746b9e8b94dd6e8ac6642fd107e1a3
Sandra Chung <sandra@moveon.org>
2016-09-16 10:34:05 -0600

    added expand parameter

--- ngpvan_api/people.py
+++ ngpvan_api/people.py
@@ -4,5 +4,5 @@ class NGPVANPeopleAPI(base.NGPVANAPI):
 
     def get_people(self, van_id):
         return self.client.get(
-            '%speople/%s' % (self.base_url, van_id)
+            '%speople/%s?$expand=phones,emails,addresses,customFields' % (self.base_url, van_id)
         )
