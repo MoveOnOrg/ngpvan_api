aed8a9bfd3355a0ef8ff845fe920ee71417f0f21
Sandra Chung <sandra@moveon.org>
2016-09-16 08:29:11 -0600

    some tweaks for consistency

--- ngpvan_api/people.py
+++ ngpvan_api/people.py
@@ -2,7 +2,7 @@ from ngpvan_api import base
 
 class NGPVANPeopleAPI(base.NGPVANAPI):
 
-    def get_people(self, path, vanid):
+    def get_people(self, van_id):
         return self.client.get(
-            '%s%s/%s' % (self.base_url, path, vanid)
+            '%speople/%s' % (self.base_url, van_id)
         )
