f0bbd4e464961728cb744b98c16f91c9b9560a07
Sandra Chung <sandra@moveon.org>
2016-09-16 11:00:06 -0600

    changed argument name

--- ngpvan_api/people.py
+++ ngpvan_api/people.py
@@ -4,6 +4,6 @@ class NGPVANPeopleAPI(base.NGPVANAPI):
 
     def get_people(self, mycampaign_id):
         response = self.client.get(
-            '%speople/%s?$expand=phones,emails,addresses,customFields' % (self.base_url, van_id)
+            '%speople/%s?$expand=phones,emails,addresses,customFields' % (self.base_url, mycampaign_id)
         )
         return {'results': response, 'items': mycampaign_id}
