d5bba7de474e1b329d90b8b660057b9df9f8ad6a
Sandra Chung <sandra@moveon.org>
2016-09-16 10:47:15 -0600

    tweaked people.py for consistency

--- ngpvan_api/people.py
+++ ngpvan_api/people.py
@@ -2,7 +2,8 @@ from ngpvan_api import base
 
 class NGPVANPeopleAPI(base.NGPVANAPI):
 
-    def get_people(self, van_id):
-        return self.client.get(
+    def get_people(self, my_campaigns_id):
+        response = self.client.get(
             '%speople/%s?$expand=phones,emails,addresses,customFields' % (self.base_url, van_id)
         )
+        return {'results': response, 'items': my_campaigns_id}
